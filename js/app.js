2bd9b914b23afdd5aec57aec38d3b8555b87d9e2
LukeWall <Wallluke06@gmail.com>
2024-05-17 08:48:04 -0400

    .

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var outputbox = document.getElementById('total')
+var input
+
+var price
+
+function pickle(){
+  if(document.getElementById('smallpickle').checked == true){
+    let price = 2
+    let cost = input*price
+    return cost
+  }
+}
+
+
+
+
+el.cost = cost.toString()
+outputbox.innerText = el.cost
+
+
+
+
+
